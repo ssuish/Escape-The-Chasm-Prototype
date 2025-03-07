3a530e9f8d1803a466e71ae73ad5bc55e01bfe5c
BlazenAkali <jhoscuenca15@gmail.com>
2025-03-07 10:12:40 +0800

    Player health

--- src/game/UIComponents/PlayerHealthBar.ts
+++ src/game/UIComponents/PlayerHealthBar.ts
@@ -1,27 +1,47 @@
+import { GameObjects, Scene } from "phaser";
 import { Player } from "../entities/Player";
-import { Component } from "react";
 
-export default class PlayerHealthBar extends Component{
-    player: Player
-    healthBar: Phaser.GameObjects.Rectangle;
-    playerHealth: number
-    playerMaxHealth: number
 
-    constructor( player: Player ){
-        super({ key: 'PlayerHealthBar' }); 
-        this.player = player
+export default class PlayerHealthBar extends GameObjects.Graphics {
+    barWidth: number;
+    barHeight: number;
+    player:  Player;
+    constructor(scene: Scene, x: number, y: number, player: Player) {
+        super(scene, { x: x, y: y });
+        this.scene = scene;
+        this.player = player;
+        this.barWidth = 100;
+        this.barHeight = 20;
+
+        this.draw();
+        scene.add.existing(this);
+        this.setScrollFactor(0); // Fix it to the camera
     }
 
-    create(){
-        //this.healthBar = this.add.rectangle(400, 300, 50, 50, 0xffffff)
-        
-        this.playerMaxHealth = this.player.GetMaxHealth();
-        
-        console.log(this.playerMaxHealth);
+    draw() {
+        this.clear();
+
+        // Background
+        this.fillStyle(0x000000);
+        this.fillRect(0, 0, this.barWidth, this.barHeight);
+
+        // Health fill
+        let percent = this.player.GetHealth() / this.player.GetMaxHealth();
+        let fillWidth = this.barWidth * percent;
+
+        let color = 0xFF0000; // Red
+        if (percent > 0.5) {
+            color = 0xFFFF00; // Yellow
+        }
+        if (percent > 0.75) {
+            color = 0x00FF00; // Green
+        }
+
+        this.fillStyle(color);
+        this.fillRect(0, 0, fillWidth, this.barHeight);
     }
-    enemyHitPlayer(){
-        //enemy-hit(damage)  key(parameter)
-        this.playerHealth = this.player.GetHealth();
-        console.log(this.playerHealth);
+
+    update() {
+        this.draw();
     }
 }
